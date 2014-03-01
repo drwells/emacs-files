5fefa37c32a65fe12209079936e19f83868941b4
David Wells <drwells@vt.edu>
2014-03-01 13:13:33 -0500

    Let org-mode work with windmove.

--- orgmaster.el
+++ orgmaster.el
@@ -1,4 +1,4 @@
-; extra stuff for org-mode
+;; extra stuff for org-mode
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
@@ -6,6 +6,12 @@
 (global-set-key "\C-cb" 'org-iswitchb)
 (setq org-log-done t) ; display timestamp when I complete something
 (setq org-agenda-files '("~/org/work.org" "/org/school.org" "/org/home.org"))
+
+;; use windmove.
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
 
 (defun org-surround-latex (start end)
   "surround the current region with #+BEGIN_SRC and #+END_SRC lines."
