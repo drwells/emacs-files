870f6b7f35ba36b6c853dac1b129df25c6131806
David Wells <drwells@vt.edu>
2014-11-15 10:11:55 -0500

    switch from ELPA to MELPA, part 1.

--- elpa/dash-2.5.0/dash-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (21219 61374
-;;;;;;  803703 847000))
-
-;;;***
-
-(provide 'dash-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; dash-autoloads.el ends here
