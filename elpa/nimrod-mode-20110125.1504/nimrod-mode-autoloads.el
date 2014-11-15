9bbc58c1ae27651c75b0667864b7d45add223c3f
David Wells <drwells@vt.edu>
2014-11-15 10:13:44 -0500

    Added new MELPA code.

---
+++ elpa/nimrod-mode-20110125.1504/nimrod-mode-autoloads.el
@@ -0,0 +1,16 @@
+;;; nimrod-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("nimrod-mode.el") (21583 37208 597815
+;;;;;;  25000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; nimrod-mode-autoloads.el ends here
