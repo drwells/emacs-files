faba1d639cf6a99a0b0b1b45af89e84c9f43b408
David Wells <drwells@vt.edu>
2015-12-19 13:26:25 -0500

    updated SMEX

--- elpa/smex-20151212.1409/smex-autoloads.el
+++ elpa/smex-20151212.1409/smex-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "smex" "smex.el" (21644 33706 439656 547000))
+;;;### (autoloads nil "smex" "smex.el" (22133 39352 103691 764000))
 ;;; Generated autoloads from smex.el
 
 (autoload 'smex "smex" "\
